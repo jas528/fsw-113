33febd77e890373e09fd215840c2704a2a9eb1a9
jas528 <jasmine.pipkins@bryanuniversity.edu>
2021-06-07 11:08:07 -0500

    wk1

---
+++ index.js
@@ -0,0 +1,31 @@
+var   students = [
+    {
+        
+        
+        name: "John", City: "Califoronia"},
+        {name: "Peter",City: "London"}, 
+        {name: "Mathew",City : "Manchester"},
+        {name: "Jane",City: "Phoenix"},
+    ]
+
+students. foeEach(element=> console.log(element));
+   
+students[0].dateOfBirth- "01/31/1980", students [1].dateOfBirth-"01/30/1981", students[2].dateOfBirth-"02/15/1998",students[3].dateOfBirth-"11/10/13";
+students[0].state="Califoronia", students [1].state="London", students[2].state-"United Kingdom",students[3].state="Arizona";
+students[0].payRate = "$15.00/hour", students[1].payrate= "$16.00/hour", students[2].payrate= "$17.00/hour", students[3].payrate= "$18.00/hour";
+students.forEach(element=> console.log(element));
+
+const gradStudents=[
+    {name: "John",City: "Califoronia"},
+    { name: "Peter", City :"London"}, 
+    { name: "Mathew",City : "Manchester"},
+    { name: "Jane",City: "Phoenix"},   
+]
+gradstudents. forEach(element=> console.log(element));   
+
+students[0].dateOfBirth- "02/31/1980", students [1].dateOfBirth-"02/30/1981", students[2].dateOfBirth-"03/15/1998",students[3].dateOfBirth-"12/10/13";
+students[0].state="Califoronia", students [1].state="London", students[2].state-"United Kingdom",students[3].state="Arizona";
+students[0].payRate = "$18.00/hour", students[1].payrate= "$19.00/hour", students[2].payrate= "$27.00/hour", students[3].payrate= "$28.00/hour";
+students.forEach(element=> console.log(element));      
+
+gradstudents. forEach(element=> console.log(element)); 
