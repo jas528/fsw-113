74a453d5d293aa118227a4b0faaad0ee912ec538
jas528 <jasmine.pipkins@bryanuniversity.edu>
2021-06-07 11:27:22 -0500

    wk1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var   students = [
     {
         
         
-        name: "John", City: "Califoronia"},
+        name: "John", City: "Califoronia"},git 
         {name: "Peter",City: "London"}, 
         {name: "Mathew",City : "Manchester"},
         {name: "Jane",City: "Phoenix"},
