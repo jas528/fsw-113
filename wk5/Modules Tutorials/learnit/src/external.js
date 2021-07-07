63c378c65dec469c63643c336eaf210b01cad40a
jas528 <jasmine.pipkins@bryanuniversity.edu>
2021-07-07 10:54:26 -0500

    wk5

---
+++ wk5/Modules Tutorials/learnit/src/external.js
@@ -0,0 +1,15 @@
+  //Clo1: 
+  //number=10
+  //console.log (name) => returns an error,name is not defined
+
+
+// Clo4:,Clo5
+"use strict"
+//Clo6:
+var number=10
+//Clo 7:
+//exports.Number=number
+ //Clo8:
+ var name = "John"
+exports.Number  = number
+exports.Name  = name
---
+++ wk5/Modules Tutorials/learnit/src/main.js
@@ -0,0 +1,15 @@
+//Clo2: 
+const ext =require("./external.js")
+
+
+//CLo3:
+//console.log("from ext",number)// error number is not defined;
+ //Clo4 ,Clo5
+//console.log("from ext",number)// error number is not defined;
+//CLo6:
+//console.log("from ext",number)// error number is not defined;
+//CLo7:
+//console.log("from ext",ext.Number)// from ext 10 
+// Clo8:
+//console.log("from ext", ext.Number)//from ext 10
+//console.log("from ext", ext.Name)// from ext John
