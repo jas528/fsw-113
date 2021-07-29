5136519a400ae0130c690e4643334d6df3c078c6
jas528 <76809910+jas528@users.noreply.github.com>
2021-07-29 12:29:02 -0500

    functions

--- week77/demo.js
+++ week77/demo.js
@@ -1,18 +1,37 @@
 
 
 
-const fetch = require('node-fetch')
-const starWarsApi = 'https://swapi.dev/api/'
+const fetch = require("node-fetch");
+const starWarApi = 'https://swapi.dev/api/'
 
-const starWarCitizenURL = `$(starWarApi)/people/1`
- 
-    fetch (starWarCitizenURL,{
+const starWarCitizenURL1 = `${starWarApi}/people/1`
+const starWarCitizenURL2 = `${starWarApi}/people/2`
+
+    fetch1 (starWarCitizenURL,{
     method: 'GET',
-    heasders:{
+    headers: {
         "Content-Type": "application/json"
     }
     })
-        .then(response=>response.json())
+        .then(
+            response=>response.json()
+            )
+       .then(data=>{
+        console.log('Sucess:', data);
+    })
+.catch ((error)=> {
+    console.error('Error:',error);
+})
+
+fetch2 (starWarCitizenURL,{
+    method: 'GET',
+    headers:{
+        "Content-Type": "application/json"
+    }
+    })
+        .then(
+            response=>response.json()
+            )
        .then(data=>{
         console.log('Sucess:', data);
     })
